d8f0d7e8d2832ca4a1bfd9bb3788de8e46d75dab
Dar0909 <egorova.d4scha@yandex.ru>
2024-06-21 21:46:58 +0800

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    public static void main(String[] args) {
+    public static void Calculator (String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите первое число:");
